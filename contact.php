073db4fc338dac7e728fddbb5b4a2c5194945e80
James Cool <jeffreysbrother@gmail.com>
2015-08-27 12:17:41 -0700

    escaped output, styled and corrected error messages

--- contact.php
+++ contact.php
@@ -1,57 +1,57 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$name = trim($_POST["name"]);
-$email = trim($_POST["email"]);
-$phone = trim($_POST["phone"]);
-$message = trim($_POST["message"]);
+  $name = trim($_POST["name"]);
+  $email = trim($_POST["email"]);
+  $phone = trim($_POST["phone"]);
+  $message = trim($_POST["message"]);
 
-if ($name == "" OR $email == "" OR $message == ""){
-echo "You must first specify a value for the NAME, EMAIL, and MESSAGE fields";
-exit;
+  if ($name == "" OR $email == "" OR $message == ""){
+  $error_message = "You must first specify a value for the NAME, EMAIL, and MESSAGE fields.";
+  }
+
+  if (isset($error_message)) {
+    foreach( $_POST as $value ){
+      if(stripos($value, 'Content-Type:') !== FALSE ){
+        $error_message = "There was a problem with the information submitted.";
+      }
+    }
+  }
+
+  if (!isset($error_message) && $_POST["address"] != "") {
+  $error_message = "Your form submission has an error.";
+  }
+
+  require_once("inc/class.phpmailer.php");
+  $mail = new PHPMailer();
+
+  //this is not working!
+  if (!isset($error_message) && !$mail->ValidateAddress($email)){
+  $error_message = "You must specify a valid email address.";
+  }
+
+  if (!isset($error_message)) {
+      $email_body = "";
+      $email_body = $email_body . "Name: " . $name . "<br />";
+      $email_body = $email_body . "Email: " . $email . "<br />";
+      $email_body = $email_body . "Message: " . $message;
+
+      $mail->SetFrom($email, $name);
+      $address = "jeffreysbrother@gmail.com";
+      $mail->AddAddress($address, "jeffreysbrother");
+      $mail->Subject    = "jeffreysbrother: Contact Form Submission | " . $name;
+      $mail->MsgHTML($email_body);
+
+      if($mail->Send()){
+          header("Location: contact.php?status=thanks");
+          exit;
+      } else {
+        $error_message = "There was a problem sending the email: " . $mail->ErrorInfo;
+      }
+    }
 }
 
-foreach( $_POST as $value ){
-if(stripos($value, 'Content-Type:') !== FALSE ){
-  echo "There was a problem with the information submitted.";
-  exit;
-}
-}
 
-if ($_POST["address"] != "") {
-echo "Your form submission has an error.";
-exit;
-}
-
-require_once("inc/class.phpmailer.php");
-$mail = new PHPMailer();
-
-//this is not working!
-if (!$mail->ValidateAddress($email)){
-echo "You must specify a valid email address.";
-exit;
-}
-
-$email_body = "";
-$email_body = $email_body . "Name: " . $name . "<br />";
-$email_body = $email_body . "Email: " . $email . "<br />";
-$email_body = $email_body . "Message: " . $message;
-
-$mail->SetFrom($email, $name);
-$address = "jeffreysbrother@gmail.com";
-$mail->AddAddress($address, "jeffreysbrother");
-$mail->Subject    = "jeffreysbrother: Contact Form Submission | " . $name;
-$mail->MsgHTML($email_body);
-
-if(!$mail->Send()){
-echo "There was a problem sending the email: " . $mail->ErrorInfo;
-exit;
-}
-
-//redirect after we send the email
-header("Location: contact.php?status=thanks");
-exit;
-}
 ?>
 
 <!DOCTYPE html>
@@ -87,27 +87,29 @@ exit;
 
 <h2>Contact</h2>
 
-
 <?php
 if (isset($_GET["status"]) AND $_GET["status"] == "thanks") { ?>
         <h1>Thanks for the email!</h1>
 <?php } else { ?>
 
-
-            <!-- <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p> -->
+            <?php
+              if (isset($error_message)) {
+                echo '<p class="message">' . $error_message . '</p>';
+              }
+            ?>
 
             <form method="post" action="contact.php">
 
                         <p>
-                            <input class="form1" type="text" name="name" id="name" placeholder="name">
+                            <input class="form1" type="text" name="name" id="name" placeholder="name" value="<?php if (isset($name)) { echo htmlspecialchars($name); }?>">
                         </p>
 
                         <p>
-                            <input class="form1" type="text" name="email" id="email" placeholder="email">
+                            <input class="form1" type="text" name="email" id="email" placeholder="email" value="<?php if (isset($email)) { echo htmlspecialchars($email); }?>">
                         </p>
 
                         <p>
-                            <textarea name="message" id="message" placeholder="message me!"></textarea>
+                            <textarea name="message" id="message" placeholder="message me!"><?php if (isset($message)) { echo htmlspecialchars($message); }?></textarea>
                         </p>
 
                         <p style="display: none;">
