0709536f90d798b6895774adaa18ab4deb9b6248
James Cool <jeffreysbrother@gmail.com>
2015-08-28 18:31:00 -0700

    deleted old stylesheet, fixed font rules

--- inc/header.php
+++ inc/header.php
@@ -17,7 +17,6 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/custom.css">
-  <!-- <link rel="stylesheet" href="css/style.css"> -->
 </head>
 <body>
 
