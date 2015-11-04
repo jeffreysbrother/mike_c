54e44ad2d9f8ab84d5a08addc8494c393f2acba4
James Cool <jeffreysbrother@gmail.com>
2015-11-03 22:26:58 -0800

    grammatical upgrade on biography page

--- biography.php
+++ biography.php
@@ -12,7 +12,7 @@ include('inc/header.php');
 
       <p>Michael Chambers is a composer, songwriter, and producer.</p>
 
-      <p>At an early age Michael was captivated by film music and it's powerful psychological impact on human emotions. He began teaching himself to play piano by ear after listening to and watching his older sister practice. He would often plunk out his favorite movie themes and create variations around them. This sparked a new passion for composition. Over the years he would begin branching out musically, teaching himself guitar and drums while writing songs and leading several bands.</p>
+      <p>At an early age Michael was captivated by film music and its powerful psychological impact on human emotions. He began teaching himself to play piano by ear after listening to and watching his older sister practice. He would often plunk out his favorite movie themes and create variations around them. This sparked a new passion for composition. Over the years he would begin branching out musically, teaching himself guitar and drums while writing songs and leading several bands.</p>
 
       <p>After graduating from Berklee College of Music, Chambers moved back to his hometown, Los Angeles and started the band The Swell Dive. He has scored several short films, advertisements, and web-series.</p>
 
